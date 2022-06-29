565a5362507b4c4c2b1b9830148807f09517481a
marianogarialdi <marianogarialdi23@gmail.com>
2022-06-29 20:21:16 -0300

    fixes en el guardado.php

--- guardado.php
+++ guardado.php
@@ -1,10 +1,9 @@
 <?php
 
 $host = "localhost";
-$username = "root";
+$username = "rot";
 $pass = "";
 $database = "usuarios";
-
 
 $conexion = mysqli_connect($host, $username, $pass, $database);
 
@@ -12,15 +11,20 @@ $conexion = mysqli_connect($host, $username, $pass, $database);
 if (!$conexion) {
 	echo "No se pudo conectar al servidor";
 }
-else
-{
-$nombre = $_POST["inombre"];
-$apellido = $_POST["iapellido"];
-$email = $_POST["iemail"];
-$telefono = $_POST["itelefono"];
+else{
+	$nombre = $_POST["inombre"];
+	$apellido = $_POST["iapellido"];
+	$email = $_POST["iemail"];
+	$telefono = $_POST["itelefono"];
+	
 
-$sql = "INSERT INTO usuariosdb (nombre,apellido,email,telefono) VALUES ('$nombre','$apellido','$email','$telefono')";
-
+	$sql = "INSERT INTO usuariosdb (nombre,apellido,email,telefono) VALUES ('$nombre','$apellido','$email','$telefono')";
+	//si la conexion es exitosa, ejecutamos la consulta
+	if ($conexion->query($sql) === TRUE) {
+		echo "Entrada creada con exito";
+	}else {
+		echo "Error: " . $sql . "<br>" . $conexion->error;
+	}
 }
 
 
